2fd2a923233dc3359e3814aa88cd3d98d8a10bea
Dokki <51916722+Dokki3@users.noreply.github.com>
2022-10-16 00:01:10 +0300

    0.1.2 menu (buttons)

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def hello_world():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        input_1 = request.form['input1']
+        input_2 = request.form['input2']
+
+        if input_1 == '':
+            button_input_1 = 'display:none'
+        else:
+            button_input_1 = ''
+
+        if input_2 == '':
+            button_input_2 = 'display:none'
+        else:
+            button_input_2 = ''
+
+        return render_template('index.html', v1=input_1, v2=input_2,
+                               button_input_1=button_input_1, button_input_2=button_input_2)
 
 
 if __name__ == '__main__':
