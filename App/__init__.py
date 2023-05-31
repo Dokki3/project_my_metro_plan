a6c7e9b9333192e2b420928591d9263a5b7d8ed5
dokki <dimkh003@gmail.com>
2023-05-31 17:38:16 +0300

    0.5.2 map

--- App/__init__.py
+++ App/__init__.py
@@ -3,8 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
-manager = LoginManager(app)
 
 from App import routs
